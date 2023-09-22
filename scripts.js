18d4d070151b791b0472cb82a69b53a9941155b4
erhan <ye.koral@gmail.com>
2023-09-23 01:47:33 +0200

    serch bar added

--- scripts.js
+++ scripts.js
@@ -14,10 +14,14 @@ const speciesBtn = document.querySelector('#species');
 const starshipsBtn = document.querySelector('#starships');
 const vehiclesBtn = document.querySelector('#vehicles');
 const bar = document.querySelectorAll("span")
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
 
 homeIcon.addEventListener("click", () =>{
     console.log("homeIcon clicked");
     collageContainer.className = 'collage-container';
+    const collectionSection = document.querySelector('#collection-container');
+    collectionSection.className = "collection-container-hidden";
 })
 
 toggleIcon.addEventListener('click', () =>{
@@ -27,13 +31,56 @@ toggleIcon.addEventListener('click', () =>{
     })
 })
 
+const searchHandler = () => {
+    const collectionSection = document.querySelector('#collection-container');
+    if(collageContainer.className === 'collage-container') { 
+        collageContainer.className = 'collage-container-hidden';
+    } else if(collectionSection) {
+        collectionSection.remove();
+    }
+    
+    const searchText = searchInput.value.toLowerCase()
+
+    const newSection = document.createElement('section')
+    newSection.id = 'collection-container';
+    newSection.className = 'collection-container';
+    Object.keys(data).map(key => {
+        data[key].map(item => {
+            if (searchText && item.name.toLowerCase().includes(searchText)) {
+                console.log(item.name);
+                const newDiv = document.createElement('div');
+                newDiv.className = 'card';
+                const newImg = document.createElement('img');
+                newImg.src = item.src;
+                newDiv.appendChild(newImg);
+                const newP = document.createElement('p');
+                const newText = document.createTextNode(item.name)
+                newP.appendChild(newText);
+                newDiv.appendChild(newP);
+                newSection.appendChild(newDiv);
+                main.appendChild(newSection);
+            } else if (searchText && !item.name.toLowerCase().includes(searchText)){
+                if(collectionSection) {
+                    collectionSection.remove();
+                }
+            }else {
+                collectionSection.remove();
+                collageContainer.className = 'collage-container';
+            }
+        })
+        })
+
+    } 
+
 const clickHandler = (e) => {
     collageContainer.className = 'collage-container-hidden';
     let collection = e.target.id;
     const collectionSection = document.querySelector('#collection-container');
-    collectionSection.remove();
+    if(collectionSection) {
+        collectionSection.remove();
+    }
     const newSection = document.createElement('section')
-        newSection.id = 'collection-container';
+    newSection.id = 'collection-container';
         newSection.className = 'collection-container';
     data[collection].map(item => {
         const newDiv = document.createElement('div');
@@ -50,27 +97,14 @@ const clickHandler = (e) => {
     })
 }
 
+searchInput.addEventListener('input', searchHandler)
+searchButton.addEventListener("click", searchHandler)
 peopleBtn.addEventListener('click', clickHandler );
 speciesBtn.addEventListener('click', clickHandler );
 starshipsBtn.addEventListener('click', clickHandler );
 vehiclesBtn.addEventListener('click', clickHandler );
 
-// speciesBtn.addEventListener('click', () => {
-//     data.species.map(person => {
-//     const collectionSection = document.querySelector('#collection-container');
-//     const newDiv = document.createElement('div');
-//     newDiv.className = 'card';
-//     const newImg = document.createElement('img');
-//     newImg.src = person.src;
-//     newDiv.appendChild(newImg);
-//     const newP = document.createElement('p');
-//     const newText = document.createTextNode(person.name)
-//     newP.appendChild(newText);
-//     newDiv.appendChild(newP);
-//     collectionSection.appendChild(newDiv);
-        
-//     })
-// });
+
 
 
 
